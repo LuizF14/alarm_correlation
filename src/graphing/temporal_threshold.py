780f075a971223a2ff09911b9d2daafc0324ced5
LuizF14 <54507701+LuizF14@users.noreply.github.com>
2026-02-13 12:02:15 -0300

    Resultados tao bao ein

--- src/graphing/temporal_threshold.py
+++ src/graphing/temporal_threshold.py
@@ -25,3 +25,23 @@ class TemporalThreshold(GraphingBase):
 
                 graph.add_edge(a['Alert ID'], b['Alert ID'])
         return graph
+    
+    def graph_by_key(self, data, key, threshold: pd.Timedelta = pd.Timedelta(minutes=5)):
+        graph = nx.DiGraph()
+
+        data = data.sort_values("First Occurrence").reset_index()
+        unique_values = data[key].unique()
+
+        graph.add_nodes_from(unique_values)
+
+        for i in range(len(data)):
+            a = data.iloc[i]
+            
+            for j in range(i+1,len(data)):
+                b = data.iloc[j]
+
+                if b["First Occurrence"] > a["Last Occurrence"] + threshold:
+                    break
+
+                graph.add_edge(a[key], b[key])
+        return graph
